5f0f7afb0e530efad848a985b6ec7f8c3316dcfd
DominikaStefaniak <161628339+DominikaStefaniak@users.noreply.github.com>
2024-10-16 23:09:20 +0200

    main and functions update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-from preprocessing_functions import column_count_dicts_with_condition
+from preprocessing_functions import count_matching_dicts
 
-dataframe = pd.read_json("C:/Users/domci/ml-solvro-recruitment1/cocktail_dataset.json")
+dataframe = pd.read_json("C:/Users/domci/PycharmProjects/ml-solvro-recruitment/cocktail_dataset.json")
 
-df = column_count_dicts_with_condition(dataframe, "ingredients", "alcohol", 1, "alcoholic_ingredients")
+dataframe["alcoholic_ingredients"] = dataframe["ingredients"].apply(count_matching_dicts, args=("alcohol", 1))
 
-print(df)
+print(dataframe)
--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -1,5 +1,5 @@
 
-def lists_to_categorical(df, outside_column, categorical_key, condition_key, condition_value):
+def list_of_dictionaries_unique_categories(df, outside_column, categorical_key, condition_key, condition_value):
     unique_categories = set()
     # find distinct values in the categorical column
     for list_of_dictionaries in df[outside_column]:
@@ -8,30 +8,13 @@ def lists_to_categorical(df, outside_column, categorical_key, condition_key, con
                 unique_categories.add(dictionary[categorical_key])
 
     return unique_categories
-'''
-    # carry out one-hot-encoding on categorical column
-    for category in unique_categories:
-        df[category] = df[outside_column].apply(
-            lambda list_of_dictionaries: 1 if any(
-                ingredient[categorical_key] == category for ingredient in list_of_dictionaries
-            ) else 0
-        )
-
-    return df
-'''
-
-def column_count_dicts_with_condition(df, column_name, key, value, new_column_name):
-
-    def count_matching_dicts(list_of_dicts):
-        number_of_matching_dicts = 0
-        if isinstance(list_of_dicts, list):
-            for dict in list_of_dicts:
-                if dict[key] == value:
-                    number_of_matching_dicts += 1
-
-        return number_of_matching_dicts
 
 
-    df[new_column_name] = df[column_name].apply(count_matching_dicts)
+def count_matching_dicts(list_of_dicts, key, value):
+    number_of_matching_dicts = 0
+    if isinstance(list_of_dicts, list):
+        for dict in list_of_dicts:
+            if dict[key] == value:
+                number_of_matching_dicts += 1
 
-    return df
+    return number_of_matching_dicts
