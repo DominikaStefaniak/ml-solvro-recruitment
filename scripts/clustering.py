1bae62ad5eeec534680a84a46c836c15d1621f6f
DominikaStefaniak <161628339+DominikaStefaniak@users.noreply.github.com>
2024-10-28 12:20:27 +0100

    add: find best n_components, quantitative evaluation

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -3,24 +3,67 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import umap
 from umap import UMAP
+from sklearn.metrics import silhouette_score, davies_bouldin_score
+import os
 
+# To avoid user warning coming from KMeans model
+os.environ['OMP_NUM_THREADS'] = '1'
+
+# Import the prepared data
 df = pd.read_parquet("../data/final_cocktail_dataset.parquet")
 
-# Scale the data to improve the effect of the model
+# Scale the data to improve the efficiency of the models
 scaler = StandardScaler()
 df[["numIngredients", "numAlcoholicIngredients"]] = scaler.fit_transform(df[["numIngredients", "numAlcoholicIngredients"]])
 
-# Exclude numeric data for dimensionality reduction and clustering model
+# Exclude numeric data for dimensionality reduction
 numeric_data = df.select_dtypes(include=['int64', 'float64'])
 
-# Apply umap for dimensionality reduction
-reducer = umap.UMAP(n_components=2, random_state=42)
-umap_embedding = reducer.fit_transform(numeric_data)
+# Apply umap for dimensionality reduction to 10 components
+reducer10D = UMAP(n_components=10, random_state=42, n_jobs=1)
+umap_data = reducer10D.fit_transform(numeric_data)
 
-df_umap = pd.DataFrame(umap_embedding, columns=['UMAP1', 'UMAP2'])
+# Create a new dataframe with columns reduced by umap
+df_umap = pd.DataFrame(umap_data, columns=[f"UMAP{i+1}" for i in range(10)])
 
-# KMeans
-kmeans = KMeans(n_clusters=4, random_state=42)
-df_umap["clusters"] = kmeans.fit_predict(df_umap)
+# Search for the best performance of KMeans for different number of clusters using both the Silhouette and Bouldin metric
+best_score = -1 # Worst possible score
+best_n_score = 2 # Lowest possible
 
-df_umap.to_parquet("../data/final_cocktail_dataset_with_clusters.parquet")
+for number in range(2, 11):
+    kmeans = KMeans(n_clusters=number, random_state=42)
+    clusters = kmeans.fit_predict(df_umap)
+
+    silhouette_avg = silhouette_score(df_umap, clusters)
+    davies_bouldin = davies_bouldin_score(df_umap, clusters)
+
+    # It's best when the Silhouette metric is close to 1 and Bouldin metric close to 0
+    # Strive for the biggest difference of these two metrics
+    if silhouette_avg - davies_bouldin > best_score:
+        best_score = silhouette_avg - davies_bouldin
+        best_n_score = number
+
+print(best_score, best_n_score)
+
+# Use KMeans with the number of clusters that performed best according to the used metrics
+kmeans = KMeans(n_clusters=best_n_score, random_state=42)
+df_umap["KMeans_clusters"] = kmeans.fit_predict(df_umap)
+
+# Evaluate the final KMeans model
+silhouette_avg = silhouette_score(df_umap.iloc[:, :-1], df_umap["KMeans_clusters"])
+davies_bouldin = davies_bouldin_score(df_umap.iloc[:, :-1], df_umap["KMeans_clusters"])
+
+# Print the results of the used metrics
+print(f'Number of clusters for best KMeans performance: {best_n_score}')
+print(f'Silhouette score: {silhouette_avg}, Davies Bouldin score: {davies_bouldin}')
+
+# Prepare the data for quality evaluation
+reducer2D = UMAP(n_components=2, random_state=42, n_jobs=1)
+umap_2d = reducer2D.fit_transform(df_umap.iloc[:, :-1])
+
+# Convert to DataFrame for saving
+df_umap_2d = pd.DataFrame(umap_2d, columns=["UMAP1", "UMAP2"])
+df_umap_2d["KMeans_clusters"] = df_umap["KMeans_clusters"]
+
+# Save the dataframe
+df_umap_2d.to_parquet("../data/final_cocktail_dataset_with_clusters.parquet")
--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -23,7 +23,7 @@ df = generalize_items_in_column(df, "glass", 3, "Other")
 # Encode generalized 'glass' column
 df = one_hot_encode_column(df, "glass")
 
-# Exclude a binary(0 or 1) column IBA from 'tags' column
+# Exclude a binary(0 or 1) column "IBA" from "tags" column
 df = encode_one_category_from_lists(df, "tags", "IBA")
 
 # Create a column that contains an information about the number of used ingredients in each cocktail
@@ -32,21 +32,18 @@ df["numIngredients"] = df["ingredients"].apply(len)
 # Create a column that shows how many of the cocktails ingredients are alcoholic
 df["numAlcoholicIngredients"] = df["ingredients"].apply(count_matching_dicts, args=("alcohol", 1))
 
+# Generalize some of the most used ingredients
+#df = generalize_values_list_of_dicts(df, "ingredients", "name", "Sugar additive", ["Powdered Sugar", "Sugar"])
+#df = generalize_values_list_of_dicts(df, "ingredients", "name", "Lemon additive", ["Lemon Juice", "lemon", "Lemon Peel"])
+#df = generalize_values_list_of_dicts(df, "ingredients", "name", "Lime addictive", ["Lime", "Lime Juice"])
+
 # Encode most used ingredients in the ingredients column
 sorted_items_counter = sorted_column_items_counter(df, "ingredients", "name")
 
-for ingredient in list(sorted_items_counter.keys()) [:10]:
+for ingredient in list(sorted_items_counter.keys()) [:20]:
     df[ingredient] = df["ingredients"].apply(
         lambda ingredients_list: 1 if any(ingredient_dict["name"] == ingredient for ingredient_dict in ingredients_list) else 0
     )
-
-# Change columns names to standardize them
-df.columns = ['id', 'name', 'ingredients', 'ordinaryDrink',
-      'glassChampagneFlute', 'glassCocktailGlass', 'glassCollinsGlass',
-       'glassHighballGlass', 'glassOld-fashionedGlass', 'glassOther',
-       'glassWhiskeySour', 'IBA', 'numIngredients', 'numAlcoholicIngredients', 'gin', 'lightRum',
-        'tripleSec', 'sugar', 'lemonJuice', 'lemon', 'powderedSugar',
-        'lemonPeel', 'lime', 'dryVermouth']
 
 # Save the final dataframe to parquet so that it saves the information about columns datatypes
 df.to_parquet("../data/final_cocktail_dataset.parquet")
